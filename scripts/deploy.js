2579be92f8e2e878d877965b46ed62bb6fb916c5
carlos <gg.20twenty1@gmail.com>
2023-04-22 23:59:57 -0400

    deploy script js

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,32 +1,67 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
-// will compile your contracts, add the Hardhat Runtime Environment's members to the
-// global scope, and execute the script.
-const hre = require("hardhat");
+const { ethers } = require("hardhat");
+const fs = require("fs");
+const bemaTokenSource = require("./path/to/BEMA.sol");
 
-async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
+async function deployContracts(options) {
+  const {
+    bemaToken: bemaTokenName = "BemaToken",
+    stakingPool: stakingPoolName = "StakingPool",
+    distribution: distributionName = "Distribution",
+    bemaMusic: bemaMusicName = "BemaMusic",
+    bemaSale: bemaSaleName = "BemaSale"
+  } = options;
 
-  const lockedAmount = hre.ethers.utils.parseEther("0.001");
+  const [deployer] = await ethers.getSigners();
+  console.log("Deploying contracts with the account:", deployer.address);
 
-  const Lock = await hre.ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
+  // Deploy BemaToken contract
+  const BemaToken = await ethers.getContractFactory(bemaTokenSource.abi, bemaTokenSource.bytecode);
+  const bemaToken = await BemaToken.deploy();
+  console.log(`${bemaTokenName} contract deployed to:`, bemaToken.address);
+  fs.writeFileSync(`${bemaTokenName.toLowerCase()}_abi.json`, JSON.stringify(BemaToken.interface.abi, null, 2));
 
-  await lock.deployed();
+  // Call the mint function of BemaToken contract to mint 100 tokens
+  await bemaToken.mint(deployer.address, ethers.utils.parseEther("100"));
 
-  console.log(
-    `Lock with ${ethers.utils.formatEther(
-      lockedAmount
-    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
+  // Deploy StakingPool contract
+  const StakingPool = await ethers.getContractFactory(stakingPoolName);
+  const stakingPool = await StakingPool.deploy(bemaToken.address);
+  console.log(`${stakingPoolName} contract deployed to:`, stakingPool.address);
+  fs.writeFileSync(`${stakingPoolName.toLowerCase()}_abi.json`, JSON.stringify(StakingPool.interface.abi, null, 2));
+
+  // Deploy BemaMusic contract
+  const BemaMusic = await ethers.getContractFactory(bemaMusicName);
+  const bemaMusic = await BemaMusic.deploy();
+  console.log(`${bemaMusicName} contract deployed to:`, bemaMusic.address);
+  fs.writeFileSync(`${bemaMusicName.toLowerCase()}_abi.json`, JSON.stringify(BemaMusic.interface.abi, null, 2));
+
+  // Deploy BemaSale contract
+  const BemaSale = await ethers.getContractFactory(bemaSaleName);
+  const bemaSale = await BemaSale.deploy(bemaMusic.address);
+  console.log(`${bemaSaleName} contract deployed to:`, bemaSale.address);
+  fs.writeFileSync(`${bemaSaleName.toLowerCase()}_abi.json`, JSON.stringify(BemaSale.interface.abi, null, 2));
+
+  // Deploy Distribution contract
+  const Distribution = await ethers.getContractFactory(distributionName);
+  const distribution = await Distribution.deploy(
+    bemaToken.address,
+    stakingPool.address,
+    bemaMusic.address,
+    bemaSale.address
   );
+  console.log(`${distributionName} contract deployed to:`, distribution.address);
+  fs.writeFileSync(`${distributionName.toLowerCase()}_abi.json`, JSON.stringify(Distribution.interface.abi, null, 2));
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
-main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
+// If script is being run directly, deploy the contracts
+if (require.main === module) {
+  deployContracts({})
+    .then(() => process.exit(0))
+    .catch((error) => {
+      console.error(error);
+      process.exit(1);
+    });
+}
+
+// Export the deployContracts function so it can be imported and reused in other scripts
+module.exports = deployContracts;
